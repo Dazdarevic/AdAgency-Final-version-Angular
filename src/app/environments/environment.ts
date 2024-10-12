ce021288f10c091763637d86dde4743906cc53d1
Dazdarevic <belkisa.dazdarevic@gmail.com>
2024-10-12 16:26:31 -0700

    link

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   baseUrl: "http://harisbronja-002-site2.jtempurl.com/api",
-  username: "belina-001",
-  password: "Informacioni.1"
+  username: "",
+  password: ""
 };
